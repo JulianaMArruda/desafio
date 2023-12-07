05361db959aef8caab8fd458833c9aa01d6ec0c4
JulianaMArruda <139576858+JulianaMArruda@users.noreply.github.com>
2023-12-06 21:56:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+console.log("Ano novo está chegando!!")
+
+//----///
+console.log( "Mariah Carey")
+
+
+
+ //---///
+
+let nascimento = 1969
+if(nascimento == 1969) {
+    console. log("Sinto muito, mas assistirá da tv globo")
+  }else{
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.")
+  }
+
+  
+  /////------////
+
+  
+  let mes = junho
+
+  if(mes = julho){
+   console.log("Algo de errado não está certo, o mês digitado foi o mês junho")
+  } else if (mes == junho){
+   console.log("junho é o mês certo")
+  } else if (mes ===setembro ) {
+     console.log(" uma ou mais opções estão corretas")
+  }
+
+  ///===////
+
